a30440b019d8111abca2a28742bc42377088408a
Yeonhee59 <dusgml05099@gmail.com>
2024-09-03 15:13:05 +0900

    2주차

--- src/main/java/inhatc/cse/yeonheeshop/hello/controller/HelloController.java
+++ src/main/java/inhatc/cse/yeonheeshop/hello/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     @GetMapping("/")
     public  String hello(Model model){
         HelloDto helloDto = HelloDto.builder().
-                name("최길동")
+                name("최길동e")
                 .build();
 
         model.addAttribute("data", helloDto);
